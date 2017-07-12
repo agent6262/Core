9fb2f646b9f2c10ebb5a082b2a3250d9d868ab8d
Tyler <tylerb318@gmail.com>
2017-07-11 21:20:02 -0500

    Forgot to rename the storage adapter for last commit.

--- constants.php
+++ constants.php
@@ -7,6 +7,8 @@ const CORE_VERSION = '0.1.0.0';
 
 /**
  * Class AdapterList Holds a list of all adapters that are default and explicitly loaded.
+ * @author Tyler Bucher <tylerb318@gmailcom>
+ * @since 0.1.0.0
  */
 abstract class AdapterList {
 
@@ -14,6 +16,6 @@ abstract class AdapterList {
      * @var array The array of adapters that this library will load by default.
      */
     public static $adapters = array(
-        'StorageAdapter.class' => array('name' => 'system')
+        'StorageAdapter' => array('name' => 'system')
     );
 }
